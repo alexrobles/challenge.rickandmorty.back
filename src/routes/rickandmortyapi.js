43ba4cc579d5d5b93caa72884aabb03688849236
alexrobles <jeisonrp25@gmail.com>
2024-01-05 18:40:25 -0500

    (feat) adding new getCaharcters endpoint

--- src/routes/rickandmortyapi.js
+++ src/routes/rickandmortyapi.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 const router = require('express').Router();
-
+const {messageRequest } = require('../utils/messageRequest');
 const { getCharacterIdsAsFavorites, createCharacter }  = require('../database/characters');
 
 const apiURL = process.env.RICK_AND_MORTY_URL
@@ -16,10 +16,23 @@ router.post("/createCharacter", async (req, res) => {
    });
 
 router.get("/getCharacters", async (req, res) => {
-    const result = await axios.get(`${apiURL}/`);
-    res.json(result.data);
-   }
-);
+  try {
+    const page = req.query.page;
+    const gender= req.query.gender;
+    const species= req.query.species;
+  
+    const result = await axios.get(`${apiURL}/character/?page=${page}&gender=${gender}&species=${species}`);
+    return  res.status(200).send(messageRequest("Ejecución correcta","","",result.data));
+     
+  } catch (error) {
+    res.status(error.response.status).send({
+      message: error.response.data.error,
+      code: error.code,
+      state: error.response.status,
+      data: []
+  });
+  }
+  });
 
 
 module.exports = router;
